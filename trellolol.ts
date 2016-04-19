6d50b4a751c172b154ae590e54874a7c1468b9cb
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-04-19 16:26:52 -0400

    Add ref path :lightning:

--- trellolol.ts
+++ trellolol.ts
@@ -1,69 +1,11 @@
 'use strict';
+/// <reference path="lib/trelloItems.ts"/>
 
 declare var require: any
 const fs = require('fs');
 const json2md = require('json2md');
 
-class TrelloCard {
-  name: string;
-  desc: string;
 
-  constructor(name: string, desc: string) {
-    this.name = name;
-    this.desc = desc;
-  }
-
-  toMarkdown(): string {
-    return '\n- ' + this.name;
-  }
-}
-
-class TrelloList {
-  name: string;
-  cards: TrelloCard[];
-
-  constructor(name: string, cards: TrelloCard[]) {
-    this.name = name;
-    this.cards = cards;
-  }
-
-  stringifyCards(): string[] {
-    return this.cards.map(card => card.toMarkdown());
-  }
-
-  toJSON(): any {
-    return {
-      h3: this.name
-    };
-  }
-
-  toMarkdown(): string {
-    return json2md(this.toJSON()) + this.stringifyCards();
-  }
-}
-
-class TrelloDocument {
-  title: string;
-  lists: TrelloList[];
-
-  constructor(title: string, lists: TrelloList[]) {
-    this.title = title;
-    this.lists = lists;
-  }
-
-  toJSON(): any {
-    return {
-      h1: this.title
-    };
-
-  }
-
-  toMarkdown(): string {
-    let header = json2md(this.toJSON());
-    let compiledLists = this.lists.map(list => '\n' + list.toMarkdown());
-    return header.concat(compiledLists);
-  }
-}
 
 /// Set up us the Trello objects.
 const trelloBoard = require('./example.json');
