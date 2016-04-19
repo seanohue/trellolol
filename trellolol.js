6d6960c28eabaa09d8bb818721e92e4acd3e2804
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-04-18 22:09:09 -0400

    Now finds lists based on the target list. :shipit:

--- trellolol.js
+++ trellolol.js
@@ -1,5 +1,9 @@
 'use strict';
 var fs = require('fs');
+/// Set up us the Trello objects.
 var trelloBoard = require('./example.json');
 var cards = trelloBoard.cards;
-console.log(cards);
+var lists = trelloBoard.lists;
+var targetListNames = ['Technical'];
+var targetLists = lists.filter(function (list) { return targetListNames.indexOf(list.name) > -1; });
+console.log(targetLists);
--- trellolol.ts
+++ trellolol.ts
@@ -1,8 +1,13 @@
 'use strict';
 
 declare var require: any
-
 const fs = require('fs');
+
+/// Set up us the Trello objects.
 const trelloBoard = require('./example.json');
-const cards = trelloBoard.cards;
-console.log(cards);
+const cards: any[] = trelloBoard.cards;
+const lists: any[] = trelloBoard.lists;
+const targetListNames: string[] = ['Technical'];
+const targetLists: any[] = lists.filter(list => targetListNames.indexOf(list.name) > -1);
+
+console.log(targetLists);
