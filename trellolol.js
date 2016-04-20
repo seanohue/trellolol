591c5cf28bccbcdc1f349dfb829839433df64549
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-04-19 21:57:08 -0400

    Configure commander, fix commander, try to add scripts to package, use built-in array functions :book:

--- trellolol.js
+++ trellolol.js
@@ -1,23 +1,35 @@
 'use strict';
 var Trello = require('./lib/trelloItems');
 var fs = require('fs');
+var commander = require('commander');
+// Defaults
+var inputFile = './example.json';
+var outputFile = './example.md';
+var targetListName = 'Adding game features';
+// CLI options
+commander
+    .arguments('<input file>', '<output file>', '<list name>')
+    .action(function (input, output, name) {
+    inputFile = input.endsWith('.json') ? input : input + '.json';
+    outputFile = output.endsWith('.md') ? output : output + '.md';
+    targetListName = name || targetListName;
+})
+    .parse(process.argv);
 /// Set up us the Trello objects.
-var trelloBoard = require('./example.json');
+var trelloBoard = require(inputFile);
 var cards = trelloBoard.cards;
 var lists = trelloBoard.lists;
 //TODO: Pull from board name.
-var projectName = 'Schmup';
+var projectName = trelloBoard.name || 'Project';
 // Organize the Trello objects.
-var targetListNames = ['Adding game features'];
-var targetLists = lists.filter(function (list) { return contains(targetListNames, list.name); });
+var targetListNames = [targetListName];
+var targetLists = lists.filter(function (list) { return targetListNames.some(function (name) { return name === list.name; }); });
 var targetListIDs = targetLists.map(function (list) { return list.id; });
-var targetCards = cards.filter(function (card) { return contains(targetListIDs, card.idList); });
+var targetCards = cards.filter(function (card) { return targetListIDs.some(function (id) { return id === card.idList; }); });
 // Put objects into classes...
 var myCards = targetCards.map(function (card) { return new Trello.Card(card.name, card.desc); });
 var myLists = targetLists.map(function (list) { return new Trello.List(list.name, myCards); });
 var myDocument = new Trello.Document(projectName, myLists);
+console.log("\nRendered unto Markdown thusly:\n");
 console.log(myDocument.toMarkdown());
-fs.writeFileSync('./example.md', myDocument.toMarkdown());
-function contains(collection, item) {
-    return collection.indexOf(item) > -1;
-}
+fs.writeFileSync(outputFile, myDocument.toMarkdown());
--- trellolol.ts
+++ trellolol.ts
@@ -1,6 +1,7 @@
 'use strict';
 /// <reference path="lib/trelloItems.ts"/>
 declare var require: any;
+declare var process: any;
 
 import Trello = require('./lib/trelloItems');
 
@@ -10,13 +11,15 @@ const commander = require('commander');
 // Defaults
 let inputFile = './example.json';
 let outputFile = './example.md';
+let targetListName = 'Adding game features';
 
 // CLI options
 commander
-  .arguments('<input file>', '<output file>')
-  .action((input, output) => {
+  .arguments('<input file>', '<output file>', '<list name>')
+  .action((input, output, name) => {
     inputFile = input.endsWith('.json') ? input : input + '.json';
     outputFile = output.endsWith('.md') ? output : output + '.md';
+    targetListName = name || targetListName;
   })
   .parse(process.argv);
 
@@ -27,10 +30,10 @@ const cards: any[] = trelloBoard.cards;
 const lists: any[] = trelloBoard.lists;
 
 //TODO: Pull from board name.
-const projectName: string = 'Schmup';
+const projectName: string = trelloBoard.name || 'Project';
 
 // Organize the Trello objects.
-const targetListNames: string[] = ['Adding game features'];
+const targetListNames: string[] = [targetListName];
 const targetLists: any[] = lists.filter(list => targetListNames.some(name => name === list.name));
 const targetListIDs: string[] = targetLists.map(list => list.id);
 const targetCards: any[] = cards.filter(card => targetListIDs.some(id => id === card.idList));
@@ -40,6 +43,7 @@ let myCards: Trello.Card[] = targetCards.map(card => new Trello.Card(card.name, 
 let myLists: Trello.List[] = targetLists.map(list => new Trello.List(list.name, myCards));
 let myDocument: Trello.Document = new Trello.Document(projectName, myLists);
 
+console.log("\nRendered unto Markdown thusly:\n");
 console.log(myDocument.toMarkdown());
 
 fs.writeFileSync(outputFile, myDocument.toMarkdown());
