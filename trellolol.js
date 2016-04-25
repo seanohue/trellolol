0ff54574ecc89ec7d7aa6b1eeca89454daec3484
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-04-25 09:40:30 -0400

    Instead of doing the archived thing (since none of the cards are closed...), look for cards within 30 days.

--- trellolol.js
+++ trellolol.js
@@ -11,7 +11,7 @@ var targetListName;
 // CLI options
 commander
     .arguments('<input> <output> [listname]')
-    .option('-a, --archived', 'Include archived cards')
+    .option('-n, --newer', 'Only include cards from the past 30 days')
     .action(function (input, output, listname) {
     [input, output].forEach(validate);
     inputFile = (input.endsWith('.json') ? input : input + '.json');
@@ -37,7 +37,20 @@ var targetListNames = [targetListName];
 var targetLists = lists.filter(function (list) { return targetListNames.some(function (name) { return name === list.name; }); });
 var targetListIDs = targetLists.map(function (list) { return list.id; });
 var targetCards = cards.filter(function (card) { return targetListIDs.some(function (id) { return id === card.idList; }); })
-    .filter(function (card) { return commander.archived ? true : card.closed === 'true'; });
+    .filter(function (card) {
+    if (!commander.newer)
+        return true;
+    var base = 10;
+    var today = new Date();
+    var month = today.getMonth() + 1;
+    var day = today.getDay() + 1;
+    var cardMonth = parseInt(card.dateLastActivity.substring(5, 7), base);
+    var cardDay = parseInt(card.dateLastActivity.substring(8, 10), base);
+    var isSameMonth = cardMonth === month;
+    var isPreviousMonth = cardMonth === cardMonth - 1;
+    var isWithinThirty = isPreviousMonth && cardDay >= day;
+    return isSameMonth || isWithinThirty;
+});
 // Put objects into classes...
 var myCards = targetCards.map(function (card) { return new Trello.Card(card.name, card.desc); });
 var myLists = targetLists.map(function (list) { return new Trello.List(list.name, myCards); });
--- trellolol.ts
+++ trellolol.ts
@@ -18,7 +18,7 @@ let targetListName;
 // CLI options
 commander
   .arguments('<input> <output> [listname]')
-  .option('-a, --archived', 'Include archived cards')
+  .option('-n, --newer', 'Only include cards from the past 30 days')
   .action((input, output, listname) => {
     [input, output].forEach(validate);
     inputFile = (input.endsWith('.json') ? input : input + '.json');
@@ -47,7 +47,22 @@ const targetListNames: string[] = [targetListName];
 const targetLists: any[] = lists.filter(list => targetListNames.some(name => name === list.name));
 const targetListIDs: string[] = targetLists.map(list => list.id);
 const targetCards: any[] = cards.filter(card => targetListIDs.some(id => id === card.idList))
-                                .filter(card => commander.archived ? true : card.closed === 'true');
+
+                                // Filter out cards not from last 30 days.
+                                .filter(card => {
+                                  if (!commander.newer) return true;
+                                  const base = 10;
+                                  const today = new Date();
+                                  const month = today.getMonth() + 1;
+                                  const day = today.getDay() + 1;
+                                  const cardMonth = parseInt(card.dateLastActivity.substring(5, 7), base);
+                                  const cardDay = parseInt(card.dateLastActivity.substring(8, 10), base);
+
+                                  let isSameMonth     = cardMonth === month;
+                                  let isPreviousMonth = cardMonth === cardMonth - 1;
+                                  let isWithinThirty  = isPreviousMonth && cardDay >= day;
+                                  return isSameMonth || isWithinThirty;
+                                });
 
 // Put objects into classes...
 let myCards: Trello.Card[] = targetCards.map(card => new Trello.Card(card.name, card.desc));
