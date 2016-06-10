c27e37224d784380619d7daa241c65115c221e46
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-06-10 15:50:27 -0400

    Add useful comments :book: to MD.js

--- md.js
+++ md.js
@@ -1,6 +1,12 @@
 'use strict';
+
+// Makes a paragraph in MD by leading with a newline.
 const p = str => '\n' + str;
+
+// Makes a list item by leading with a newline and -
 const li = str => p('- ' + str);
+
+// Makes a header by leading with a newline and a number of #'s according to how header-y it is.
 const h = (n, str) => p([...Array(n)].map(() => '#')) + ' ' + str;
 
 
